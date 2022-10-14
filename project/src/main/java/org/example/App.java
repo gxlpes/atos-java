5c7450182a93347e7de69410039553e512aabe03
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-10-14 10:11:36 -0300

    Update and removing data

--- project/src/main/java/org/example/App.java
+++ project/src/main/java/org/example/App.java
@@ -7,21 +7,33 @@ import javax.persistence.Persistence;
 public class App {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com"); // id da pessoa é null já que o BD vai incrementar os IDs
-        Pessoa p2 = new Pessoa(null, "Roberto da Silva", "roberto@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Yasmin da Silva", "yasmin@gmail.com");
+//        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com"); // id da pessoa é null já que o BD vai incrementar os IDs
+//        Pessoa p2 = new Pessoa(null, "Roberto da Silva", "roberto@gmail.com");
+//        Pessoa p3 = new Pessoa(null, "Yasmin da Silva", "yasmin@gmail.com");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("agenda-jpa"); // instancia o entity manager com config do persistence.xml
         EntityManager em = emf.createEntityManager(); // contexto de persistência e conexão com o banco
 
-        em.getTransaction().begin(); // começo da transação
+//        em.getTransaction().begin(); // começo da transação
 
-        em.persist(p1); // método persist que vai salvar o conteúdo dentro do banco de dados
-        em.persist(p2);
-        em.persist(p3);
+//        em.persist(p1); // método persist que vai salvar o conteúdo no banco de dados
+//        em.persist(p2);
+//        em.persist(p3);
 
-        em.getTransaction().commit(); // commit da transação
+//        em.getTransaction().commit(); // commit da transação
+
+        // buscar conteúdos e objetos por ID no banco de dados
+//        Pessoa p = em.find(Pessoa.class, 2);
+//        System.out.println(p);
+
+        // para apagar uma pessoa do banco de dados deve-se chamar o método remove()
+        Pessoa p = em.find(Pessoa.class, 2); // recuperando a entidade que se deseja excluir
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
 
         System.out.println("Implementação realizada com sucesso!");
+        em.close(); // fechar o entity manager
+        emf.close(); // fechar
     }
 }
