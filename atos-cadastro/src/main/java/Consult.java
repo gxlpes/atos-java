e9357033bf10d387a1404600d97cdf3fbeb6bea3
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-10-16 19:33:21 -0300

    Updates in project

---
+++ atos-cadastro/src/main/java/Consult.java
@@ -0,0 +1,31 @@
+import javax.persistence.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+
+public class Consult {
+
+    public static void all() throws IOException {
+        Scanner read = new Scanner(System.in);
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("atos_cadastro"); // instancia o entity manager com config do persistence.xml
+        EntityManager em = emf.createEntityManager(); // contexto de persistência e conexão com o banco
+
+        String jpql = "select u from Aluno u";
+        TypedQuery<Aluno> query = em.createQuery(jpql, Aluno.class);
+        query.setMaxResults(40);
+
+        List<Aluno> alunos = query.getResultList();
+
+        for (Aluno aluno : alunos) {
+            System.out.println("ID: " + aluno.getId() + " | " + "Nome: " + aluno.getNome());
+        }
+
+        em.close();
+        emf.close();
+
+    }
+
+}
---
+++ atos-cadastro/src/main/java/Insert.java
@@ -0,0 +1,50 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+public class Insert {
+    static String dec;
+
+
+    public static void novoAluno() throws IOException {
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("atos_cadastro"); // instancia o entity manager com config do persistence.xml
+        EntityManager em = emf.createEntityManager(); // contexto de persistência e conexão com o banco
+        Scanner read = new Scanner(System.in);
+
+        do {
+            System.out.println("Digite o CPF");
+            String cpf = read.nextLine();
+            System.out.println("Digite o nome completo");
+            String nome = read.nextLine();
+            System.out.println("Digite o email");
+            String email = read.nextLine();
+            System.out.println("Digite o estado");
+            String estado = read.nextLine();
+            System.out.println("Digite a cidade");
+            String cidade = read.nextLine();
+
+            Aluno student = new Aluno(null, cpf, nome, email, estado, cidade);
+            App.list.add(student);
+
+            em.getTransaction().begin(); // começo da transação
+            em.persist(student);
+            em.getTransaction().commit(); // commit da transação
+
+
+            System.out.println("Você deseja continuar?");
+            dec = read.nextLine();
+
+        } while (dec.equals("sim") || dec.equals("Sim"));
+
+        System.out.println("Implementação realizada com sucesso!");
+        App.padrao();
+
+        em.close(); // fechar o entity manager
+        emf.close(); // fechar
+        read.close();
+    }
+}
