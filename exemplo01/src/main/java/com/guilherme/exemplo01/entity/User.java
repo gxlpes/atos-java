db00f1a1fcf00452d4b30cc1f95215fa920e5294
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-10-21 21:32:01 -0300

    New project login

---
+++ exemplo01/src/main/java/com/guilherme/exemplo01/entity/User.java
@@ -0,0 +1,30 @@
+package com.guilherme.exemplo01.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name="id_user")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="username", nullable = false)
+    private String username;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+
+}
