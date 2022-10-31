0099b4abb3989e6ef98eee4c501d37dc3d56e231
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-10-31 20:26:25 -0300

    First spring project

---
+++ exemploOneToOne/introd/src/test/java/com/example/introd/IntrodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
