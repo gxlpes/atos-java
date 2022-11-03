6286e0ce3ca2be2e6c1cfe151a7c928b7d36fcba
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-11-03 19:53:13 -0300

    New Spring project

---
+++ exemploDB/src/test/java/com/example/exemplodb/ExemploDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemplodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
